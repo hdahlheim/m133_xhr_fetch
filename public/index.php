d0a6dd02eec1e3590f44a5aea524fd9d8eff7872
hactar <hactar@cyberkraft.ch>
2020-02-17 22:55:47 +0100

    Inital commit

---
+++ public/index.php
@@ -0,0 +1,50 @@
+<?php
+
+require '../vendor/autoload.php';
+
+use \Siler\Route;
+use \Siler\Http\Response;
+
+Route\get('/', '../temp/index.phtml');
+
+Route\get('/ajax/html/students', function () {
+    $students = getStudents();
+
+    $template = template(
+        '../temp/_students.phtml',
+        ['students' => $students]
+    );
+
+    Response\html($template);
+});
+
+Route\get('/ajax/json/students', function () {
+    $students = getStudents();
+
+    Response\json($students);
+});
+
+if (!Route\did_match()) {
+    Response\html('404 not found', 404);
+}
+
+
+
+
+
+
+
+function getStudents () {
+    return json_decode(file_get_contents('../students.json'), true);
+}
+
+function template($path, $context) {
+    // Provide the context variables in the current context
+    extract($context);
+
+    // Load template as string
+    ob_start();
+    require $path;
+
+    return ob_get_clean(); // Return template string
+}
---
+++ public/js/main.js
@@ -0,0 +1,52 @@
+// function loadStudents () {
+//   const xhr = new XMLHttpRequest()
+
+//   const div = document.querySelector('[data-js-students]')
+
+//   xhr.open('GET', '/ajax/html/students')
+//   xhr.onreadystatechange = function () {
+//     if(xhr.readyState === xhr.DONE && xhr.status === 200) {
+//       div.innerHTML = xhr.responseText
+//     }
+//   }
+//   xhr.send()
+// }
+
+// function loadStudents () {
+//   const div = document.querySelector('[data-js-students]')
+
+//   fetch('/ajax/html/students')
+//     .then(res => {
+//       if (res.ok) {
+//         return res.text()
+//       }
+//     })
+//     .then(text => {
+//       div.innerHTML = text
+//     })
+// }
+
+// function loadStudents () {
+//   const div = document.querySelector('[data-js-students]')
+
+//   fetch('/ajax/json/students')
+//     .then(res => {
+//       if (res.ok) {
+//         return res.json()
+//       }
+//       throw res.statusText
+//     })
+//     .then(json => {
+//       const list = document.createElement('ul')
+
+//       json.forEach(student => {
+//         const li = document.createElement('li')
+//         li.innerText = student.name
+//         list.appendChild(li)
+//       })
+//       div.innerHTML = list.outerHTML
+//     })
+//     .catch(e => console.log(e))
+// }
+
+document.querySelector('#loadStudents').addEventListener('click', loadStudents)
